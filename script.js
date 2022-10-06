c3e3747da3eca95e1b74a66ae1219a1d171439e4
Joshua Rymer <joshuarymer1@gmail.com>
2022-10-06 17:16:10 +0100

    Add changeSize() button functionality

--- script.js
+++ script.js
@@ -1,10 +1,39 @@
 const uniCon = document.querySelector('#uni-con');
-for(i=0; i<256; i++) {
+const btn = document.querySelector('#size-select');
+
+let selectedSize = 16;
+
+for(i=0; i<(selectedSize*selectedSize); i++) {
     div = document.createElement('div');
     div.classList.add('grid');
     uniCon.appendChild(div);
 }
+
 document.querySelectorAll('.grid').forEach((div) => {
      div.addEventListener('mouseover', (e) =>
      e.target.classList.add('hover'))
      })
+
+btn.addEventListener('click', () => changeSize());
+
+function changeSize() {
+             selectedSize = prompt("Enter a integer between 1-100 to choose the square's length.");
+     
+             uniCon.innerHTML= '';
+     
+             for(i=0; i<(selectedSize*selectedSize); i++) {
+                 div = document.createElement('div');
+                 div.classList.add('grid');
+                 uniCon.appendChild(div);
+             };
+
+             document.querySelectorAll('.grid').forEach((div) => {
+                div.addEventListener('mouseover', (e) =>
+                e.target.classList.add('hover'))
+                })
+}
+                
+
+
+
+ 
